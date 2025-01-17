1df1860bb35ac7088dd24b4d66e19a95c84163a1
foodeggs-7 <lucasgaming443@gmail.com>
2025-01-17 14:49:00 +0100

    set navbar zindex to 2000 so it register clicks

--- resources/js/navbar.js
+++ resources/js/navbar.js
@@ -2,32 +2,45 @@
 
 document.addEventListener('DOMContentLoaded', function () {
 
-    const header = document.getElementById('NavBar')
-    const dateLabel = document.getElementById('Date')
-    const d = new Date();
 
-    console.log(header);
+
+
 
     fetch('https://foodeggs7.github.io/resources/topbar/navbar.html')
-    .then(response => response.text())
-    .then(data => {
-        document.getElementById('header').innerHTML = data;
-        document.addEventListener('scroll', function () {
+        .then(response => response.text())
+        .then(data => {
 
-            if (window.scrollY > 70) {
-                header.classList.add('scrolling');
-            } else {
-                header.classList.remove('scrolling');
+            if (data) {
+                
+                document.getElementById('header').innerHTML = data;
+                setTimeout(() => {
+                    const Nav = document.getElementById('NavBar')
+
+                    console.log(Nav);
+                    const dateLabel = document.getElementById('Date')
+                    const d = new Date();
+                    
+                    document.addEventListener('scroll', function () {
+
+                        if (window.scrollY > 70) {
+                            Nav.classList.add('scrolling');
+                        } else {
+                            Nav.classList.remove('scrolling');
+                        }
+
+                    })
+
+
+
+                    console.log(`Welkom! Je bekijkt deze pagina op ${d.getDate()}-${d.getMonth() + 1}-${d.getFullYear()}.`);
+                    dateLabel.innerHTML = `welcome its: ${d.getDate()}-${d.getMonth() + 1}-${d.getFullYear()}.`;
+
+                }, 3000)
+
+
             }
 
         })
-
-
-
-        console.log(`Welkom! Je bekijkt deze pagina op ${d.getDate()}-${d.getMonth() + 1}-${d.getFullYear()}.`);
-        dateLabel.innerHTML = `welcome its: ${d.getDate()}-${d.getMonth() + 1}-${d.getFullYear()}.`;
-
-    })
 
 
 })
