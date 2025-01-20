1b8a4273695f8e1069ff706d0c3eb12aa929d774
foodeggs-7 <lucasgaming443@gmail.com>
2025-01-20 14:53:59 +0100

    completed the hero section in index and started with fill up the index also added a langouge zwiths eng and dutch

--- resources/js/navbar.js
+++ resources/js/navbar.js
@@ -43,7 +43,7 @@ document.addEventListener('DOMContentLoaded', function () {
                     function changeLanguage(languageCode) {
                         Array.from(document.getElementsByClassName('lang')).forEach(function (elem) {
                             if (elem.classList.contains('lang-' + languageCode)) {
-                                 elem.style.display = 'initial';
+                                 elem.style.display = 'flex';
                             }
                             else {
                                  elem.style.display = 'none';
