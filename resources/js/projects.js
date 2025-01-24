5f61980d58bd4166720181ee7e344a413510eace
foodeggs-7 <lucasgaming443@gmail.com>
2025-01-24 14:48:58 +0100

    fixed data fetch

--- resources/js/projects.js
+++ resources/js/projects.js
@@ -3,20 +3,22 @@ document.addEventListener('DOMContentLoaded', function () {
 
 
     var params = new URLSearchParams(window.location.search); // Parses the query string
+    console.log(params)
 var id = params.get('id'); // Extracts the value of "id"
+console.log(id)
 var IdNum = Number(id)
 console.log(IdNum); // Converts "id" to a number and logs it
 
-    fetch(`https://foodeggs7.github.io/resources/api/projects/project${id}`)
-        .then(response => response.text())
+    fetch(`https://foodeggs7.github.io/resources/api/projects/project${id}.json`)
+        .then(response => response.json())
         .then(data => {
 
             if (data) {
-
+console.log(data)
                 function load() {
                     document.getElementById('Project-Title-En').textContent = data.TitleEn
                     document.getElementById('Thumb-En').src = data.thumbEn
-                    document.getElementById('Project-Info-En') = data.descriptionEn
+                    document.getElementById('Project-Info-En').textContent = data.descriptionEn
 
 
 
